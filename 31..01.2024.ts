de286cb35e851469595cdc2cac665ca5c2949a64
Nikita Rezan <Nrezag@mail.ru>
2024-01-31 18:13:36 +1000

    dvyb

---
+++ 31..01.2024.ts
@@ -0,0 +1,19 @@
+class ClassName { }
+class Point {x: number; y: number;}
+let vladix: Point = new Point();
+
+function rl(){
+    vladix.x = 1;
+    vladix.y = 2 ;
+}
+
+rl();
+
+
+class Point1 {width: number; height: number;}
+let vladix0: Point1 = new Point1();
+
+function vladixнулина(){
+    vladix0.width = 74;
+    vladix0.height = 25;
+}
---
+++ script.ts
@@ -0,0 +1,6 @@
+let a:number = 5;
+let b:string = "hi";
+const c:string[] = ["a","b","c"]
+const arrow: () => void = function () {
+    
+}
